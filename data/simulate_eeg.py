5ece3ee400c740aa6e9863ae3f54f68e9999288d
surajsedai <surajsedai05@gmail.com>
2026-02-02 23:40:26 -0500

    Dataset & DataLoader

--- data/simulate_eeg.py
+++ data/simulate_eeg.py
@@ -36,3 +36,42 @@ def simulate_eeg_sample(
         signal[ch] = wave + noise
 
     return signal
+import torch
+from torch.utils.data import Dataset
+from utils.preprocessing import normalize_channels, window_signal
+
+class SimulatedEEGDataset(Dataset):
+    def __init__(
+        self,
+        num_samples_per_class: int,
+        num_channels: int,
+        total_timesteps: int,
+        window_size: int,
+        stride: int
+    ):
+        self.windows = []
+        self.labels = []
+
+        for class_id in range(4):
+            for _ in range(num_samples_per_class):
+                signal = simulate_eeg_sample(
+                    class_id=class_id,
+                    num_channels=num_channels,
+                    num_timesteps=total_timesteps
+                )
+
+                signal = normalize_channels(signal)
+                windows = window_signal(signal, window_size, stride)
+
+                for w in windows:
+                    self.windows.append(w)
+                    self.labels.append(class_id)
+
+        self.windows = torch.tensor(self.windows, dtype=torch.float32)
+        self.labels = torch.tensor(self.labels, dtype=torch.long)
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        return self.windows[idx], self.labels[idx]
